edf15d40a35db5f3052598a42738a5c90fc001c7
dengyuaner <dengyuaner@163.com>
2017-03-24 14:18:47 +0800

    Initial commit

---
+++ app/src/main/java/com/dy/gesture/LargeImageView.java
@@ -0,0 +1,222 @@
+package com.dy.gesture;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by dy on 2017/3/22.
+ */
+
+public class LargeImageView extends View implements GestureDetector.OnGestureListener {
+    private static final String TAG = "LargeImageView";
+
+    private BitmapRegionDecoder mDecoder;
+
+    /**
+     * 绘制的区域
+     */
+    private volatile Rect mRect = new Rect();
+
+    private int mScaledTouchSlop;
+
+    // 分别记录上次滑动的坐标
+    private int mLastX = 0;
+    private int mLastY = 0;
+    /**
+     * 图片的宽度和高度
+     */
+    private int mImageWidth, mImageHeight;
+    private GestureDetector mGestureDetector;
+    private BitmapFactory.Options options;
+
+    public LargeImageView(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public LargeImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public LargeImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+
+        mScaledTouchSlop = ViewConfiguration.get(getContext())
+                .getScaledTouchSlop();
+        Log.d(TAG, "sts:" + mScaledTouchSlop);
+        //初始化手势控制器
+        mGestureDetector = new GestureDetector(context, this);
+
+        //获取图片的宽高
+        InputStream is = null;
+        try {
+            is = context.getResources().getAssets().open("timg.jpg");
+            //初始化BitmapRegionDecode，并用它来显示图片
+            mDecoder = BitmapRegionDecoder
+                    .newInstance(is, false);
+            BitmapFactory.Options tmpOptions = new BitmapFactory.Options();
+            // Grab the bounds for the scene dimensions
+            tmpOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(is, null, tmpOptions);
+            mImageWidth = tmpOptions.outWidth;
+            mImageHeight = tmpOptions.outHeight;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //把触摸事件交给手势控制器处理
+        return mGestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        mLastX = (int) e.getRawX();
+        mLastY = (int) e.getRawY();
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        int x = (int) e2.getRawX();
+        int y = (int) e2.getRawY();
+        move(x, y);
+
+        return true;
+    }
+
+    /**
+     * 移动的时候更新图片显示的区域
+     *
+     * @param x
+     * @param y
+     */
+    private void move(int x, int y) {
+        int deltaX = x - mLastX;
+        int deltaY = y - mLastY;
+        Log.d(TAG, "move, deltaX:" + deltaX + " deltaY:" + deltaY);
+        //如果图片宽度大于屏幕宽度
+        if (mImageWidth > getWidth()) {
+            //移动rect区域
+            mRect.offset(-deltaX, 0);
+            //检查是否到达图片最右端
+            if (mRect.right > mImageWidth) {
+                mRect.right = mImageWidth;
+                mRect.left = mImageWidth - getWidth();
+            }
+
+            //检查左端
+            if (mRect.left < 0) {
+                mRect.left = 0;
+                mRect.right = getWidth();
+            }
+
+            invalidate();
+        }
+        //如果图片高度大于屏幕高度
+        if (mImageHeight > getHeight()) {
+            mRect.offset(0, -deltaY);
+
+            //是否到达最底部
+            if (mRect.bottom > mImageHeight) {
+                mRect.bottom = mImageHeight;
+                mRect.top = mImageHeight - getHeight();
+            }
+
+            if (mRect.top < 0) {
+                mRect.top = 0;
+                mRect.bottom = getHeight();
+            }
+            invalidate();
+        }
+
+
+        mLastX = x;
+        mLastY = y;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        mLastX = (int) e.getRawX();
+        mLastY = (int) e.getRawY();
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        int x = (int) e2.getRawX();
+        int y = (int) e2.getRawY();
+        move(x, y);
+        return true;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //显示图片
+        Bitmap bm = mDecoder.decodeRegion(mRect, options);
+        canvas.drawBitmap(bm, 0, 0, null);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+
+        int imageWidth = mImageWidth;
+        int imageHeight = mImageHeight;
+
+        //默认显示图片的中心区域
+        mRect.left = imageWidth / 2 - width / 2;
+        mRect.top = imageHeight / 2 - height / 2;
+        mRect.right = mRect.left + width;
+        mRect.bottom = mRect.top + height;
+
+    }
+
+}
---
+++ app/src/main/java/com/dy/gesture/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dy.gesture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private int screenX, screenY;
+    private int width, height;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+}
